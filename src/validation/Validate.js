7d8b916ad41e5c4eaf7f99abde11a7edf623d352
greeneeya <suzythetravelerr@gmail.com>
2023-11-26 12:42:52 +0900

    feat: 사용자로부터 경주할 자동차의 수만큼 이름을 받는다.

--- src/validation/Validate.js
+++ src/validation/Validate.js
@@ -1,24 +1,29 @@
 import { RULES } from '../constants/constants';
 
-class Validate {
-  static isValidNameLength(name) {
-    if (
-      name.length < RULES.minNameLength ||
-      name.length > RULES.maxNameLength
-    ) {
+export default class Validate {
+  static checkAllValidations(cars) {
+    cars.forEach((car) => {
+      this.isValidNameLength(car);
+    });
+    this.isRacable(cars);
+    this.isDuplicated(cars);
+  }
+
+  isValidNameLength(car) {
+    if (car.length < RULES.minNameLength || car.length > RULES.maxNameLength) {
       throw new Error('[ERROR] 이름은 1자 이상, 5자 이하여야 합니다.');
     }
   }
 
-  static isRacable(cars) {
+  isRacable(cars) {
     if (cars.length < 2) {
       throw new Error('[ERROR] 자동차는 2대 이상이어야 합니다.');
     }
   }
 
-  static isDuplicated(names) {
-    names.forEach((name, index) => {
-      if (names.indexOf(name) !== index) {
+  isDuplicated(cars) {
+    cars.forEach((car, index) => {
+      if (cars.indexOf(car) !== index) {
         throw new Error('[ERROR] 이름은 중복될 수 없습니다.');
       }
     });
--- src/view/InputView.js
+++ src/view/InputView.js
@@ -1,10 +1,13 @@
 import { Console } from '@woowacourse/mission-utils';
+import Validate from '../validation/Validate';
 
 const InputView = {
   async getCarName() {
-    const cars = await Console.readLineAsync(
+    const user = await Console.readLineAsync(
       '경주할 자동차 이름을 입력하세요.(이름은 쉼표(,) 기준으로 구분)\n',
     );
+    const cars = user.split(',');
+    Validate.checkAllValidations(cars);
   },
 };
 export default InputView;

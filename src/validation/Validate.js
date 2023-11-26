9f53ba4dd990fcbd4c73a936cfdb2be85430b3f9
greeneeya <suzythetravelerr@gmail.com>
2023-11-26 12:34:14 +0900

    feat: 자동차가 1대 이하일 경우, 예외처리한다.

--- src/validation/Validate.js
+++ src/validation/Validate.js
@@ -6,7 +6,13 @@ class Validate {
       name.length < RULES.minNameLength ||
       name.length > RULES.maxNameLength
     ) {
-      throw new Error('[ERROR] 이름은 1자 이상, 5자 이하여야합니다.');
+      throw new Error('[ERROR] 이름은 1자 이상, 5자 이하여야 합니다.');
     }
-  },
+  }
+
+  static isRacable(cars) {
+    if (cars.length < 2) {
+      throw new Error('[ERROR] 자동차는 2대 이상이어야 합니다.');
+    }
+  }
 }
